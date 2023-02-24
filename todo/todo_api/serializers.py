b4d87c22c1a6e4338e6be8a1a168e26d6c2a4ed3
Piotr Poloczek <piotr.jan.poloczek@gmail.com>
2023-02-24 20:15:54 +0100

    add project tutorial

--- todo/todo_api/serializers.py
+++
@@ -1,8 +0,0 @@
-from rest_framework import serializers
-from todo_api.models import Todo
-
-
-class TodoSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Todo
-        fields = ["task", "completed", "timestamp", "updated", "user"]
