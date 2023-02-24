527bef14a76fc9ef29dde3c2c31f26152a84c563
Piotr Poloczek <piotr.jan.poloczek@gmail.com>
2023-02-24 20:38:34 +0100

    finish todo tutorial

--- todo/todo_api/views.py
+++ todo/todo_api/views.py
@@ -26,3 +26,46 @@ class TodoListApiView(APIView):
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+class TodoDetailApiView(APIView):
+    permission_classes = [permissions.IsAuthenticated]
+
+    def get_object(self, todo_id, user_id):
+        """
+        Helper method to get the object with given todo_id, and user_id
+        """
+        try:
+            return Todo.objects.get(id=todo_id, user = user_id)
+        except Todo.DoesNotExist:
+            return None
+        
+    def get(self, request, todo_id, *args, **kwargs):
+        """
+        Retrieves the Todo with given todo_id
+        """
+        todo_instance = self.get_object(todo_id, request.user_id)
+        if not todo_instance:
+            return Response(
+                {"res": "Object with todo id does not exists"},
+                status=status.HTTP_400_BAD_REQUEST
+            )
+        
+        serializer = TodoSerializer(todo_instance)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+    
+    def put(self, request, todo_id, *args, **kwargs):
+        """
+        Updates the todo item with given todo_id if exists
+        """
+        todo_instance = self.get_object(todo_id, request.user.id)
+        if not todo_instance:
+            return Response(
+                {"res": "Object with todo id does not exists"},
+                status=status.HTTP_400_BAD_REQUEST
+            )
+        todo_instance.delete()
+        return Response(
+            {"res": "Object deleted!"},
+            status=status.HTTP_200_OK
+        )
